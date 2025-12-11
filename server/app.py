a151167f31e6b19c9283a3861d607cf16bb8b7c4
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-12-11 11:51:57 +0000

    build: set up base template files

--- server/app.py
+++ server/app.py
@@ -1,12 +1,21 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return jsonify(message="PottyDog App")
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
