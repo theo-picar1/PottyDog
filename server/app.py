72cd110ed3f512108448d92bd023dbd8881efadd
Theo <theopicar@icloud.com>
2025-12-16 10:18:01 +0000

    feat: implement session cookies

--- server/app.py
+++ server/app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, request, redirect
-import re
+from flask import Flask, render_template, request, session
+from flask_session import Session
 from flask_bcrypt import Bcrypt
-import os
-import mysql.connector
+from datetime import timedelta
 from dotenv import load_dotenv
-import traceback
+import mysql.connector
+import re
+import os
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
 load_dotenv()
+
+# Session cookie setup
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+IS_PROD = os.environ.get("FLASK_ENV") == "production"
+
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SECURE=IS_PROD,      
+    SESSION_COOKIE_SAMESITE="Lax",
+    PERMANENT_SESSION_LIFETIME=timedelta(days=3)
+)
 
 # DB credentials
 DB_NAME = os.getenv('DB_NAME')
@@ -31,6 +43,42 @@ def get_db_connection():
 # Main page
 @app.route('/', methods=['GET'])
 def index():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        conn = None
+        cursor = None
+
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+
+            cursor.execute(
+                "SELECT * FROM users WHERE id = %s",
+                (user_id,)
+            )
+
+            user = cursor.fetchone()
+            
+            if user:
+                userData = {
+                    'username': user['username'],
+                    'dog_name': user['dog_name']
+                }
+
+                return render_template('index.html', userData=userData)
+            else:
+                session.pop('user_id', None)
+                return render_template('index.html')
+
+        except:
+            return render_template('index.html')
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+    
     return render_template('index.html')
 
 
@@ -65,7 +113,16 @@ def login():
             if not bcrypt.check_password_hash(user['password'], password):
                 return render_template('login.html', error="Invalid email or password."), 401
             
-            return render_template('index.html', user=user), 200
+            # Store user id in session when successsfull
+            session.permanent = True
+            session['user_id'] = user['id']
+
+            userData = {
+                'username': user['username'],
+                'dog_name': user['dog_name']
+            }
+            
+            return render_template('index.html', userData=userData), 200
         
         except:
             return render_template('login.html', error="An error occurred during login. Please try again."), 500
