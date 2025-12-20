41b71c257bc3665a31fd2920fefcd2b41b33b9a2
Theo <theopicar@icloud.com>
2025-12-20 21:51:05 +0000

    feat: add endpoint for updating preferences

--- server/app.py
+++ server/app.py
@@ -101,7 +101,9 @@ def settings():
     userData = {
         'user_id': session.get('user_id'),
         'username': session.get('username'),
-        'dog_name': session.get('dog_name')
+        'dog_name': session.get('dog_name'),
+        'light_mode': session.get('light_mode'),
+        'disabled_alerts': session.get('disabled_alerts')
     }
 
     return render_template('settings.html', userData=userData), 200
@@ -148,7 +150,9 @@ def update_credentials():
                 
             userData = {
                 'username': session.get('username', user['username']),
-                'dog_name': session.get('dog_name', user['dog_name'])
+                'dog_name': session.get('dog_name', user['dog_name']),
+                'disabled_alerts': session.get('disabled_alerts'),
+                'light_mode': session.get('light_mode')
             }
 
             if not updates:
@@ -160,11 +164,80 @@ def update_credentials():
             cursor.execute(query, tuple(values))
             conn.commit()
             
-            return render_template('settings.html', success="Succesfully saved changes!", userData=userData), 200
+            return render_template('settings.html', profile_success="Succesfully saved changes!", userData=userData), 200
 
         except: 
             return render_template('settings.html', error="Error trying to update credentials. Please try again!"), 500
         
+        finally:
+            if conn:
+                conn.close()
+            if cursor:
+                cursor.close()
+                
+                
+# Update preferences for website
+@app.route('/settings/preferences', methods=['PUT', 'POST'])
+def update_preferences():
+    if request.method == 'POST':
+        try:
+            conn = None
+            cursor = None
+            
+            conn = get_db_connection()
+            cursor = conn.cursor()    
+            
+            # See if user exists to begin with
+            user_id = session.get('user_id')
+            cursor.execute(
+                "SELECT id FROM users where id = %s",
+                (user_id,)
+            )
+            
+            user = cursor.fetchone()
+            if not user:
+                return redirect(url_for('login'))
+            
+            cursor.execute(
+                "SELECT * FROM settings where user_id = %s",
+                (user_id,)
+            )
+            
+            preferences = cursor.fetchone()
+            if not preferences:
+                return render_template('settings.html', error="Error fetching preferences. Please try again!"), 404
+            
+            # If checkbox was checked, it will not be None 
+            disabled_alerts = request.form.get('disabled_alerts') is not None
+            light_mode = request.form.get('light_mode') is not None
+            
+            cursor.execute(
+                """
+                UPDATE settings 
+                SET disabled_alerts = %s, light_mode = %s
+                WHERE user_id = %s
+                """,
+                (disabled_alerts, light_mode, user_id)
+            )
+            
+            conn.commit()
+            
+            # Update new preferences, if any
+            session['disabled_alerts'] = disabled_alerts
+            session['light_mode'] = light_mode
+            
+            userData = {
+                'username': session.get('username'),
+                'dog_name': session.get('dog_name'),
+                'disabled_alerts': session.get('disabled_alerts'),
+                'light_mode': session.get('light_mode')
+            }
+            
+            return render_template('settings.html', preferences_success="Successfully saved preferences!", userData=userData)
+            
+        except:
+            return render_template('settings.html', error="Error saving preferences. Please try again!"), 500
+            
         finally:
             if conn:
                 conn.close()
@@ -194,20 +267,28 @@ def login():
                 (email,)
             )
 
-            # Check if that user exists
+            # Check if that user exists or has incorrect password
             user = cursor.fetchone()
-            if not user:
-                return render_template('login.html', user_error="No existing user found."), 404
-            
-            # Check two passwords with each other
-            if not bcrypt.check_password_hash(user['password'], password):
+            if not user or not bcrypt.check_password_hash(user['password'], password):
                 return render_template('login.html', error="Invalid email or password."), 401
+            
+            cursor.execute(
+                "SELECT light_mode, disabled_alerts FROM settings WHERE user_id = %s",
+                (user['id'],)
+            )
+            
+            # Check if settings for them exist
+            userSettings = cursor.fetchone()
+            if not userSettings:
+                return render_template('login.html', error="Could not get all info. Please try again!"), 404
             
             # Store user id in session when successfull
             session.permanent = True
             session['user_id'] = user['id']
             session['username'] = user['username']
             session['dog_name'] = user['dog_name']
+            session['light_mode'] = userSettings['light_mode']
+            session['disabled_alerts'] = userSettings['disabled_alerts']
             
             return redirect(url_for('dashboard'))
         
