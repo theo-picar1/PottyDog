4dadad35d83fb584b6af1e1470a438d9353ba670
Theo <theopicar@icloud.com>
2026-04-09 10:53:04 +0100

    style: add login page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page.component';
 import { Dashboard } from './dashboard/dashboard.component';
-import { DevicePage } from './dashboard/device/device.component';
+import { DevicePage } from './device/device.component';
+import { LoginPage } from './auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LandingPage
+    component: LoginPage
   },
   {
     path: 'dashboard',
