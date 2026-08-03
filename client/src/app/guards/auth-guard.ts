7d944daecfcfc61e038f0cbb1687d1d708857c05
Theo <theopicar@icloud.com>
2026-08-03 14:10:00 +0100

    feat: implement admin login

--- client/src/app/guards/auth-guard.ts
+++ client/src/app/guards/auth-guard.ts
@@ -7,7 +7,7 @@ export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService)
   const router = inject(Router)
   
-  if (authService.isAuthenticated()) {
+  if (authService.isAuthenticated() && !authService.isAdmin()) {
     return true
   }
 
